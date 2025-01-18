2535e9e646c7d6e97bf3f8955439a5617a58ea6c
Atari <atari@rejob.co.jp>
2025-01-19 02:57:33 +0900

    add source code

---
+++ 10_swipe-view/script.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper(".swiper", {
+  grabCursor: true,
+  speed: 500,
+  effect: "slide",
+  loop: true,
+  mousewheel: {
+    invert: false,
+    sensitivity: 1,
+  },
+});
+
+swiper.enable();
